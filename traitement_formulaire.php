41aa052e5bedbaa4b0db5683dd2ae177e3c7f3f9
Baptiste <baptistecaquot@gmail.com>
2012-04-24 20:48:31 +0200

    update

--- traitement_formulaire.php
+++ traitement_formulaire.php
@@ -22,7 +22,7 @@
 		    				'baptiste_obiz',
 		    				'obiztest',
 		    				array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-		    					  PDO::ERRMODE_EXCEPTION ));
+								  PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
 		    $age = determiner_age_bdd($_POST['datenaissance']);
 		    $ville = $_POST['ville'];
